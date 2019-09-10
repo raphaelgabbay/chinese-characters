eeef186f94ae5ad63a2f387a3e8105da3ce0e6d6
raphaelgabbay <raphael.gabbay@gmail.com>
2019-09-10 17:24:04 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    // fullscreen()
+    background(100)
+    // other stuff...
+}
+
+function draw() {
+    circle(windowWidth / 2, windowHeight / 2, 100)
+}
